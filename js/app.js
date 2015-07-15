7b37f6e2de2b1bb3a0a1b6642c3de1b75631679a
iGuest <fails.to.install@gmail.com>
2015-07-14 18:50:15 -0700

    meow#

--- js/app.js
+++ js/app.js
@@ -16,3 +16,17 @@ var myApp = angular.module('myApp', ['ngRoute'])
                 controller: 'AboutController',
             })
     })
+// Landing page controller
+myApp.controller('LandingController', function($scope){
+        $scope.number = 10
+    })
+
+// About page controller
+myApp.controller('AboutController', function($scope){
+        $scope.about = "Here's some information about this page."
+    })
+
+// Content controller
+myApp.controller('ContentController', function($scope){
+        $scope.url = "http://conference.unavsa.org/wp-content/uploads/2015/06/SEA-pic.jpg"
+    })
